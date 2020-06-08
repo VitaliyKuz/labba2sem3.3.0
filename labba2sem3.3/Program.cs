8978607655daa96a360e8390854baea05bdf9805
VitaliyKuz <derasered@gmail.com>
2020-06-08 20:28:53 +0300

    labba2sem3.3

---
+++ labba2sem3.3/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.IO;
+
+namespace labba2sem3._3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+
+
+            string s = File.ReadAllText(@"C:\Users\Laptop\Desktop\input.txt");
+            
+            string[] parts = s.Split(" ", StringSplitOptions.RemoveEmptyEntries);
+            int kil = 0;
+            
+                kil += parts.Length;
+            
+            StreamWriter stream2 = new StreamWriter(@"C:\Users\Laptop\Desktop\output.txt");
+            stream2.Write(kil);
+           
+            stream2.Close();
+
+        }
+       
+    }
+}
